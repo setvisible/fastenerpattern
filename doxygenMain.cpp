121a6f2eb8ff44965e7b7574e32112ba74711c03
setvisible <sebastien.vavassori@gmail.com>
2017-02-07 19:18:45 +0100

    added doxygen file

---
+++ doxygenMain.cpp
@@ -0,0 +1,37 @@
+/* This is a dummy code file that only contains doxygen main page
+   documentation.  It has a .cxx extension so that emacs will happily
+   autoindent correctly. */
+
+/*! \mainpage FastenerPattern
+ *
+ * FastenerPattern is a solution for designing fastener patterns
+ * (i.e. *bolted* and *riveted* joints) in Aerostructures,
+ * for Windows, Mac OS X and Linux.
+ *
+ * \section intro Introduction
+ *
+ * Its core is developed in C++, based on the Qt framework.
+ * It uses the Boost::Units module.
+ *
+ * https://www.qt.io/
+ * http://www.boost.org/
+ *
+ *
+ * \section analysis Dimensional Analysis
+ *
+ * Dimensional Analysis is the analysis of relationships between different
+ * physical quantities by identifying their fundamental dimensions.
+ *
+ * FastenerPattern uses the Dimensional Analysis abilities of the Boost::Units module
+ * to verify the dimensional homogeneity of the mechanical quantities and formulas used by the software.
+ *
+ * This enables a better Verification & Validation, since the code is more robust (less permissive) and more reliable.
+ *
+ *
+ * \section license Licensing
+ *
+ * FastenerPattern is licensed under the terms of the LGPL.
+ *
+ * https://www.gnu.org/licenses/lgpl-3.0.en.html
+ *
+ */
