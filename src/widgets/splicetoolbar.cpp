b703d327e73d597e6ee27677c8b6418553e19f53
setvisible <sebastien.vavassori@gmail.com>
2017-11-01 11:18:13 +0100

    minor change

--- src/widgets/splicetoolbar.cpp
+++ src/widgets/splicetoolbar.cpp
@@ -433,7 +433,7 @@ void SpliceToolbar::fastenerRemove()
  ******************************************************************************/
 void SpliceToolbar::designSpaceAdd()
 {
-    const qreal halfwidth = 50.0;
+    const qreal halfwidth = 10.0; // mm
     DesignSpace ds;
     ds.polygon << QPointF( -halfwidth, -halfwidth)
                << QPointF( -halfwidth,  halfwidth)
